473a18c7bced85b407981aed401cf4b8b6a480f9
Josepereira08 <jpyftsunami08@gmail.com>
2020-03-26 16:27:32 -0430

    practica05-ma

--- src/class.php
+++ src/class.php
@@ -4,10 +4,12 @@ class usuario
 {
 	public $nombre;
 	public $apellido;
+	private $ci;
 
-	function __construct($nombre,$apellido)
+	function __construct($nombre,$apellido,$ci)
 	{
 		$this->nombre=$nombre;
 		$this->apellido=$apellido;
+		$this->ci=$ci;
 	}
 }
